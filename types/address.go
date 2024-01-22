4b2de93c97cfd11d318bb81c9fb784ab8600a4a5
Dong Lieu <duongduydongtq@gmail.com>
2024-01-22 17:58:07 +0700

    fff

--- types/address.go
+++ types/address.go
@@ -195,7 +195,7 @@ func AccAddressFromBech32(address string) (addr AccAddress, err error) {
 	}
 
 	bech32PrefixAccAddr := GetConfig().GetBech32AccountAddrPrefix()
-	fmt.Println(bech32PrefixAccAddr)
+	fmt.Println("sssssssss:", bech32PrefixAccAddr)
 
 	bz, err := GetFromBech32(address, bech32PrefixAccAddr)
 	if err != nil {
